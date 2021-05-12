1171dbda17b5faff064fefaabc83c18041264eee
rogerrm95 <rogerrm@live.com>
2021-05-11 22:18:37 -0300

    Add: Implementa o header da aplicacao e estilizacoes globais.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
-export default function Header() {
-  // TODO
+import Link from "next/link";
+import { ReactElement } from "react";
+
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss'
+
+export default function Header(): ReactElement {
+  return (
+    <header className={`${commonStyles.container} ${styles.headerContainer}`}>
+
+      <Link href='/'>
+        <img src="/logo.svg" alt="logo" />
+      </Link>
+
+      <span>
+        spacetraveling <strong>.</strong>
+      </span>
+
+    </header>
+  )
 }
